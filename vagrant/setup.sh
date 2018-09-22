2e795735e456a31e1c8404af9ff547dacb816079
Jonathan Bossenger <jonathanbossenger@gmail.com>
2018-09-22 10:46:24 +0200

    Workaround for the PHP gettext deprecated issues Fixes #2

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -27,6 +27,14 @@ sudo sed -i -e "s/server_name _;/server_name $SITE_NAME;/g" /etc/nginx/sites-ava
 echo "Setting xdebug IP address in PHP ini..."
 echo "xdebug.remote_host=$IP_ADDRESS" | sudo tee -a /etc/php/7.0/fpm/conf.d/user.ini
 
+## Fixing errors in php-gettext
+## Remove this once the Ubuntu package gets updated
+echo "Fixing deprecation errors in php-gettext"
+sudo sed -i 's/function StringReader/function __construct/g' /usr/share/php/php-gettext/streams.php
+sudo sed -i "s/function FileReader/function __construct/g" /usr/share/php/php-gettext/streams.php
+sudo sed -i "s/function CachedFileReader/function __construct/g" /usr/share/php/php-gettext/streams.php
+sudo sed -i 's/function gettext_reader/function __construct/g' /usr/share/php/php-gettext/gettext.php
+
 echo "Restarting web server..."
 sudo systemctl restart php7.0-fpm
 sudo systemctl restart nginx
